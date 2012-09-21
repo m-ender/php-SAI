474da5e29068f3872ba21169066e681763d84af5
mbuettner <m.buettner.d@gmail.com>
2012-09-21 09:42:55 +0200

    Rework the existing interface
    The goal of the interface changed. Instead of providing a generic HTTP
    Client interface with loads of convenience functions, the interface will
    now simply represent the existing cURL functions. Hence, the default
    implementation (Curl.php) only delegates calls. The new stub CurlMock.php
    is going to implement the interface without any HTTP requests.

--- lib/HttpClient/CurlMock.php
+++ lib/HttpClient/CurlMock.php
@@ -1,99 +1,99 @@
 <?php
 /**
- * Implements the cURL interface by simply delegating calls to the built-in cURL functions..
+ * Mocks the cURL interface without issuing any PHP requests.
  * See http://www.php.net/manual/en/book.curl.php
  **/
-class SAI_Curl
+class SAI_HttpClient_CurlMock
     implements SAI_CurlInterface
 {
 
     public function curl_close($ch)
     {
-        curl_close($ch);
+        // TODO: Implement curl_close() method.
     }
 
     public function curl_copy_handle($ch)
     {
-        return curl_copy_handle($ch);
+        // TODO: Implement curl_copy_handle() method.
     }
 
     public function curl_errno($ch)
     {
-        return curl_errno($ch);
+        // TODO: Implement curl_errno() method.
     }
 
     public function curl_error($ch)
     {
-        return curl_error($ch);
+        // TODO: Implement curl_error() method.
     }
 
     public function curl_exec($ch)
     {
-        return curl_exec($ch);
+        // TODO: Implement curl_exec() method.
     }
 
     public function curl_getinfo($ch, $opt = 0)
     {
-        return curl_getinfo($ch, $opt);
+        // TODO: Implement curl_getinfo() method.
     }
 
     public function curl_init($url = null)
     {
-        return curl_init($url);
+        // TODO: Implement curl_init() method.
     }
 
     public function curl_multi_add_handle($mh, $ch)
     {
-        return curl_multi_add_handle($mh, $ch);
+        // TODO: Implement curl_multi_add_handle() method.
     }
 
     public function curl_multi_close($mh)
     {
-        curl_multi_close($mh);
+        // TODO: Implement curl_multi_close() method.
     }
 
     public function curl_multi_exec($mh, &$still_running)
     {
-        return curl_multi_exec($mh, $still_running);
+        // TODO: Implement curl_multi_exec() method.
     }
 
     public function curl_multi_getcontent($ch)
     {
-        return curl_multi_getcontent($ch);
+        // TODO: Implement curl_multi_getcontent() method.
     }
 
     public function curl_multi_info_read($mh, &$msgs_in_queue = null)
     {
-        return curl_multi_info_read($mh, $msgs_in_queue);
+        // TODO: Implement curl_multi_info_read() method.
     }
 
     public function curl_multi_init()
     {
-        return curl_multi_init();
+        // TODO: Implement curl_multi_init() method.
     }
 
     public function curl_multi_remove_handle($mh, $ch)
     {
-        return curl_multi_remove_handle($mh, $ch);
+        // TODO: Implement curl_multi_remove_handle() method.
     }
 
     public function curl_multi_select($mh, $timeout = 1.0)
     {
-        return curl_multi_select($mh, $timeout);
+        // TODO: Implement curl_multi_select() method.
     }
 
     public function curl_setopt_array($ch, $options)
     {
-        return curl_setopt_array($ch, $options);
+        // TODO: Implement curl_setopt_array() method.
     }
 
     public function curl_setopt($ch, $option, $value)
     {
-        return curl_setopt($ch, $option, $value);
+        // TODO: Implement curl_setopt() method.
     }
 
     public function curl_version($age = CURLVERSION_NOW)
     {
-        return curl_version($age);
+        // TODO: Implement curl_version() method.
     }
 }
